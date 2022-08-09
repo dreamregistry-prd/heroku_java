38b7e11c70c34b6ed6689b5b30c8ab0f6bdce984
Jonatan Sombié <jonatan.sombie@gmail.com>
2022-08-09 16:53:19 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# heroku_java
+
+A DReAM deploy package for deploying java app to heroku.
---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "dream_env" {
+  description = "dream app environment variables to set"
+  type        = map(string)
+  default     = {}
+}
+
+variable "dream_project_dir" {
+  description = "root directory of the project sources"
+  type        = string
+}
+
+variable "build_tool" {
+  description = "build tool to use"
+  type        = string
+  default     = "gradle"
+
+  validation {
+    condition     = contains(["gradle", "java"], var.build_tool)
+    error_message = "Allowed values for input_parameter are \"gradle\", \"java\""
+  }
+}
